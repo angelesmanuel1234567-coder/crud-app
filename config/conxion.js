31bcd7c68ded1d79430489c1d6180b68a3ddf7e1
angelesmanuel1234567-coder <angelesmanuel1234567@gmail.com>
2026-04-22 17:43:23 -0500

    Update conxion.jsjushafhajks

--- config/conxion.js
+++ config/conxion.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: process.env.MYSQLHOST,
+    host: 'shinkansen.proxy.rlwy.net',
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
-    port: Number(process.env.MYSQLPORT)
+    port: 18280
 });
 
 module.exports = pool.promise();
