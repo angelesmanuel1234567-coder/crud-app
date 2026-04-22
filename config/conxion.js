6cf6cf703c46d6478efb6675a06fa1f9e980159e
angelesmanuel1234567-coder <angelesmanuel1234567@gmail.com>
2026-04-22 16:43:36 -0500

    Update conxion.jsasdfsfsd

--- config/conxion.js
+++ config/conxion.js
@@ -1,13 +1,5 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT,
-    waitForConnections: true,
-    connectionLimit: 10
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = pool.promise();
