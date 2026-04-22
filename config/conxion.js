1c7e74603a77936ad6fd2c565fee78bff17eb72e
angelesmanuel1234567-coder <angelesmanuel1234567@gmail.com>
2026-04-22 16:53:44 -0500

    Update conxion.jsajumm

--- config/conxion.js
+++ config/conxion.js
@@ -1,7 +1,5 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    uri: process.env.DATABASE_URL
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = pool.promise();
