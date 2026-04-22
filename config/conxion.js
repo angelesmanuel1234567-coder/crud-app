bdeb6954734ae65a6a2ca4087fc3255d6718a554
angelesmanuel1234567-coder <angelesmanuel1234567@gmail.com>
2026-04-22 16:25:05 -0500

    Update conxion.js bueno nuevo

--- config/conxion.js
+++ config/conxion.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'bd'
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
+    waitForConnections: true,
+    connectionLimit: 10
 });
 
 module.exports = pool.promise();
