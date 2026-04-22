32f62eb47fd026f165f68101c70fa0aaabafee08
angelesmanuel1234567-coder <angelesmanuel1234567@gmail.com>
2026-04-22 17:16:12 -0500

    Update conxion.jsxd ya me canse

--- config/conxion.js
+++ config/conxion.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool(process.env.DATABASE_URL);
+const pool = mysql.createPool(process.env.MYSQL_PUBLIC_URL);
 
 module.exports = pool.promise();
